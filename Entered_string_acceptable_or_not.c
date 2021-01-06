1318b40b550ae226ecf95b71d083613c5dd11b5a
Shreyasri21 <61237932+Shreyasri21@users.noreply.github.com>
2021-01-06 22:19:15 +0530

    Create Entered_string_acceptable_or_not.c

---
+++ Entered_string_acceptable_or_not.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{int i,n;
+char a[20];
+printf("NAME: SHREYA SRIVASTAVA");
+printf("\nCLASS: CSE 3D");
+printf("\nROLL NO: 1816110198");
+printf("\nEnter the term (to check whether it is in form of(ab*c(a+b)): ");
+scanf("%s",&a);
+n=strlen(a);
+if(a[0]=='a'&& (a[n-1]=='a'|| a[n-1]=='b') && a[n-2]=='c')
+{
+for(i=1;i<n-2;i++)
+{if(a[i]=='b')
+continue;
+else
+{printf("STRING IS REJECTED");
+exit(0);
+}
+}
+printf("STRING IS ACCEPTED");
+}
+else
+printf("STRING REJECTED");
+getch();
+}
+
